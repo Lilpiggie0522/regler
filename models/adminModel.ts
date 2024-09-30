a852754b7113ed9566c944eb5909b75c691fec88
wilson <wilsonzhu2003@gmail.com>
2024-10-01 00:33:34 +1000

    adding dbConnect function, adminModel, developer guide and github action

---
+++ models/adminModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { InferSchemaType, Schema, model } from 'mongoose';
+
+const adminSchema = new Schema({
+  adminName: {
+    type: 'string', required: true
+   },
+  email: { type: 'string', required: true, unique: true  },
+  zid: { type: 'string', required: true   },
+  password: { type: 'string', required: true, select: false },
+  courses:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true}],
+  role: {
+      type: 'string', 
+      enum: ['courseAdmin', 'tutor'],
+      message: '{VALUE} is not a valid role',
+      required: true
+    }
+});
+
+type Admin = InferSchemaType<typeof adminSchema>;
+
+export default model<Admin>('Admin', adminSchema);
