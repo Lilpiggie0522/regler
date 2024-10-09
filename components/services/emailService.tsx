1bfd09ab84fc733c76b412ae98780689f29d3360
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-10-09 19:05:38 +1100

    added more logics on handling auth code

--- components/services/emailService.tsx
+++ components/services/emailService.tsx
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (zID: string, courseCode: string): Pr
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ zid: zID, courseCode: courseCode }),
+        body: JSON.stringify({ zID: zID, courseCode: courseCode }),
       });
 
       // if (!response.ok) {
