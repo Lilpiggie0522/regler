78360c76310ac4360e55fdcf39322d359de16dad
Waner Zheng <wanerzheng@hotmail.com>
2024-10-11 21:23:34 +1100

    improved import csv front end

--- app/lecturer/page.tsx
+++ app/lecturer/page.tsx
@@ -9,6 +9,7 @@ interface DisplayData {
     group_id: string;
     group_id2: string;
     mentor: string;
+    mentor_id:string;
     email: string
 }
 
@@ -52,6 +53,10 @@ export default function LecturerPage() {
         setShowWindow(false)
     }
 
+    function handleCloseData() {
+        setShowData(false)
+    }
+
     async function sendData(formData: FormData) {
         // console.log(results.data)
         try {
@@ -77,60 +82,79 @@ export default function LecturerPage() {
     }
 
     return (
-        <div>
+        <div className="min-h-screen bg-gray-100">
             <div className="bg-yellow-400 h-20 p-5 flex justify-between items-center">
                 <h1 className="text-black text-3xl font-bold">Dashboard</h1>
-                <button className="bg-black text-white rounded-full w-32 min-h-10" onClick={handleShowWindow}>Read CSV</button>
+                <button className="bg-black text-white rounded-full w-32 min-h-10" onClick={handleShowWindow}>Import CSV</button>
             </div>
-            {showWindow && <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                <div className="bg-gray-100 min-h-[60vh] min-w-[50vw] relative flex flex-col items-cente justify-center"> 
-                    <button className="absolute top-2 right-2 text-black text-3xl" onClick={handleCloseWindow}>
-                        &times;
-                    </button>
-                    <div className="flex flex-col justify-center items-center rounded-lg">
-                        <div className="flex p-5">
-                            <input id="csv" type="file" accept=".csv" onChange={handleFileChange} />
-                            <button className="bg-gray-300 rounded-lg p-1" type="button" onClick={cancelFile}>Cancel</button>
-                        </div>
-                        <div>
-                            {uploading && <p>Uploading file, please wait</p>}
-                            <button className="bg-green-400 rounded-lg p-1" type="button" onClick={handleSubmit}>Submit</button>
-                        </div>
-                        <div className="flex justify-center p-5">
-                            {showData &&
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th className="border border-gray-300 p-2">name</th>
-                                            <th className="border border-gray-300 p-2">zid</th>
-                                            <th className="border border-gray-300 p-2">class</th>
-                                            <th className="border border-gray-300 p-2">groupName</th>
-                                            <th className="border border-gray-300 p-2">group Id</th>
-                                            <th className="border border-gray-300 p-2">group Id2</th>
-                                            <th className="border border-gray-300 p-2">mentor</th>
-                                            <th className="border border-gray-300 p-2">email</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {displayData.map((row, index) => (
-                                            <tr key={index}>
-                                                <td className="border border-gray-300 p-2">{row.name}</td>
-                                                <td className="border border-gray-300 p-2">{row.zid}</td>
-                                                <td className="border border-gray-300 p-2">{row.class}</td>
-                                                <td className="border border-gray-300 p-2">{row.groupname}</td>
-                                                <td className="border border-gray-300 p-2">{row.group_id}</td>
-                                                <td className="border border-gray-300 p-2">{row.group_id2}</td>
-                                                <td className="border border-gray-300 p-2">{row.mentor}</td>
-                                                <td className="border border-gray-300 p-2">{row.email}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>}
+
+            {showWindow && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="bg-gray-100 min-h-[40vh] min-w-[40vw] rounded-lg relative flex flex-col items-center justify-center">
+                        <button className="absolute top-2 right-3 text-black text-3xl" onClick={handleCloseWindow}>
+                            &times;
+                        </button>
+                        <div className="flex flex-col justify-center items-center rounded-lg">
+                            <div className="flex p-5 text-black">
+                                <input id="csv" type="file" accept=".csv" onChange={handleFileChange} />
+                            </div>
+
+                            <div className="flex space-x-4">
+                                <button className="bg-gray-300 text-black rounded-lg p-1" type="button" onClick={cancelFile}>Cancel</button>
+                                <button className="bg-black text-white rounded-lg p-1" type="button" onClick={handleSubmit}>Submit</button>
+                            </div>
+                            <div className="flex space-x-4">
+                                {uploading && <p className="text-black">Uploading file, please wait</p>}
+                            </div>
                         </div>
                     </div>
-
                 </div>
-            </div>}
+            )}
+
+            {showData && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="bg-gray-100 max-h-[700px] w-[80vw] rounded-lg relative flex flex-col items-center justify-center overflow-y-scroll">
+                        <button className="absolute top-2 right-3 text-black text-3xl" onClick={handleCloseData}>
+                            &times;
+                        </button>
+                        <div className="p-5 text-black w-full overflow-y-scroll">
+                            <p className="mb-4 text-lg font-bold text-center">
+                            Your CSV file has been successfully imported:
+                            </p>
+                            <table className="w-full">
+                                <thead>
+                                    <tr>
+                                        <th className="border border-gray-300 p-2">name</th>
+                                        <th className="border border-gray-300 p-2">zid</th>
+                                        <th className="border border-gray-300 p-2">class</th>
+                                        <th className="border border-gray-300 p-2">groupName</th>
+                                        <th className="border border-gray-300 p-2">group Id</th>
+                                        <th className="border border-gray-300 p-2">group Id2</th>
+                                        <th className="border border-gray-300 p-2">mentor</th>
+                                        <th className="border border-gray-300 p-2">mentor Id</th>
+                                        <th className="border border-gray-300 p-2">email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {displayData.map((row, index) => (
+                                        <tr key={index}>
+                                            <td className="border border-gray-300 p-2">{row.name}</td>
+                                            <td className="border border-gray-300 p-2">{row.zid}</td>
+                                            <td className="border border-gray-300 p-2">{row.class}</td>
+                                            <td className="border border-gray-300 p-2">{row.groupname}</td>
+                                            <td className="border border-gray-300 p-2">{row.group_id}</td>
+                                            <td className="border border-gray-300 p-2">{row.group_id2}</td>
+                                            <td className="border border-gray-300 p-2">{row.mentor}</td>
+                                            <td className="border border-gray-300 p-2">{row.mentor_id}</td>
+                                            <td className="border border-gray-300 p-2">{row.email}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    )                  
 }
