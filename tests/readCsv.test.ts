67b297905215d5d71ecd420a2117ccb860e5e802
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-10-23 14:01:27 +1100

    Implemented unit tests on readCsv functionalities, linted and passed all tests.

--- tests/readCsv.test.ts
+++ tests/readCsv.test.ts
@@ -1,8 +1,7 @@
 import { createAdminInput, createCourseInput, createStudentInput, createTeamInput } from '@/app/api/adminSystem/initialise/route'
 import * as func from '../app/api/staff/readCsv/helpers'
-import { convertedResult } from '@/app/api/staff/readCsv/helpers'
+import { convertedResult, convertFileData } from '@/app/api/staff/readCsv/helpers'
 
-jest.mock('csv-parser', () => jest.fn())
 const converted: convertedResult[] = [
     {
         name: 'piggie',
@@ -52,8 +51,8 @@ beforeEach(() => {
     students = []
     teams = []
     newCourse = {
-        courseName: '',
-        term: '',
+        courseName: courseInfo[0],
+        term: courseInfo[1],
         mentorsEmails: '',
         teams: ''
     }
@@ -255,7 +254,36 @@ describe("Tests for csv", () => {
         expect(func.validateConvertedData(converted)).toBeFalsy()
     })
 
-    test('insert admins', () => {
+})
+
+describe("read csv route", () => {
+    it('should convert a file to parsed CSV data', async () => {
+        const mockCsvData = 'Name,zid,group Name,Class,mentor,group_id,group_id2,email,mentor_email,course_admin,admin_email\
+        \npiggie0,z1234567,piggie squad0,T13B,Piggie,z1234567,T13B_Piggie0,z1234567@student.unsw.edu.au,z2232322@unsw.edu.au,\
+        real_piggie,z5414078@ad.unsw.edu.au';
+        const mockFile = new File([mockCsvData], 'test.csv', { type: 'text/csv' });
+
+        const result = await convertFileData(mockFile);
+        expect(result).toStrictEqual([
+            {
+                name: 'piggie0',
+                zid: 'z1234567',
+                groupname: 'piggie squad0',
+                class: 'T13B',
+                mentor: 'Piggie',
+                group_id: 'z1234567',
+                group_id2: 'T13B_Piggie0',
+                email: 'z1234567@student.unsw.edu.au',
+                mentor_email: 'z2232322@unsw.edu.au',
+                course_admin: 'real_piggie',
+                admin_email: 'z5414078@ad.unsw.edu.au'
+            }
+        ])
+    });
+})
+
+describe("data insertion", () => {
+    test('admin_test: insert admins', () => {
         func.insertAdmin(convertedData, courseAdmins, courseInfo)
         expect(courseAdmins).toStrictEqual([
             {
@@ -268,7 +296,7 @@ describe("Tests for csv", () => {
         ])
     })
 
-    test('insert admins twice', () => {
+    test('admin_test: insert admins twice', () => {
         func.insertAdmin(convertedData, courseAdmins, courseInfo)
         func.insertAdmin(convertedData, courseAdmins, courseInfo)
         expect(courseAdmins).toStrictEqual([
@@ -282,7 +310,7 @@ describe("Tests for csv", () => {
         ])
     })
 
-    test('insert team', () => {
+    test('team_test: insert team', () => {
         for (const row of convertedData) {
             func.insertTeam(row, teams, newCourse)
             const teamToAdd = teams.find(team => team.teamName === row.groupname)
@@ -313,4 +341,214 @@ describe("Tests for csv", () => {
         ])
     })
 
+    test('team_test: two tutors in a team', () => {
+        convertedData.push({
+            name: "hehe boi",
+            zid: "z5417509",
+            groupname: "jeffrey squad",
+            class: "T22A",
+            mentor: "test mentor",
+            group_id: "z5417505",
+            group_id2: "T22A_Danny",
+            email: "z5417509@ad.unsw.edu.au",
+            mentor_email: "hehe_boi@bbc.co.uk",
+            course_admin: "",
+            admin_email: ""
+        })
+        for (const row of convertedData) {
+            func.insertTeam(row, teams, newCourse)
+            const teamToAdd = teams.find(team => team.teamName === row.groupname)
+            func.insertTutor(row, staffAdmins, courseInfo, newCourse, teamToAdd!)
+            func.insertStudent(row, students, teamToAdd!)
+        }
+        expect(teams).toStrictEqual([
+            {
+                teamName: 'piggie squad 0',
+                studentsZids: 'z1234567,z1234568,z1234569',
+                mentorsEmails: 'z2232322@unsw.edu.au'
+            },
+            {
+                teamName: 'piggie squad 1',
+                studentsZids: 'z1234571,z1234572',
+                mentorsEmails: 'z2232323@unsw.edu.au'
+            },
+            {
+                teamName: 'doggie squad 0',
+                studentsZids: 'z1234576,z1234577',
+                mentorsEmails: 'z2232324@unsw.edu.au'
+            },
+            {
+                teamName: 'jeffrey squad',
+                studentsZids: 'z5414078,z5423255,z5450260,z5349042,z5361545,z5417505,z5417509',
+                mentorsEmails: 'cowhorse3900@outlook.com,hehe_boi@bbc.co.uk'
+            },
+        ])
+    })
+
+    test("team_test: mentor added in a team, but also mentor of next team", () => {
+        convertedData.push({
+            name: "hehe boi",
+            zid: "z5417509",
+            groupname: "jeffrey squad",
+            class: "T22A",
+            mentor: "test mentor",
+            group_id: "z5417505",
+            group_id2: "T22A_Danny",
+            email: "z5417509@ad.unsw.edu.au",
+            mentor_email: "hehe_boi@bbc.co.uk",
+            course_admin: "",
+            admin_email: ""
+        })
+        convertedData.push({
+            name: "special kid",
+            zid: "z5322112",
+            groupname: "child_prodigy",
+            class: "T22A",
+            mentor: "Danny",
+            group_id: "z5417505",
+            group_id2: "T22A_Danny",
+            email: "z5322112@ad.unsw.edu.au",
+            mentor_email: "cowhorse3900@outlook.com",
+            course_admin: "",
+            admin_email: "",
+        },)
+        convertedData.push({
+            name: "even more special kid",
+            zid: "z5555555",
+            groupname: "child_prodigy",
+            class: "T22A",
+            mentor: "test mentor",
+            group_id: "z5555555",
+            group_id2: "T22A_Danny",
+            email: "z5555555@ad.unsw.edu.au",
+            mentor_email: "hehe_boi@bbc.co.uk",
+            course_admin: "",
+            admin_email: "",
+        },)
+        for (const row of convertedData) {
+            func.insertTeam(row, teams, newCourse)
+            const teamToAdd = teams.find(team => team.teamName === row.groupname)
+            func.insertTutor(row, staffAdmins, courseInfo, newCourse, teamToAdd!)
+            func.insertStudent(row, students, teamToAdd!)
+        }
+        console.log(teams)
+        expect(teams).toStrictEqual([
+            {
+                teamName: 'piggie squad 0',
+                studentsZids: 'z1234567,z1234568,z1234569',
+                mentorsEmails: 'z2232322@unsw.edu.au'
+            },
+            {
+                teamName: 'piggie squad 1',
+                studentsZids: 'z1234571,z1234572',
+                mentorsEmails: 'z2232323@unsw.edu.au'
+            },
+            {
+                teamName: 'doggie squad 0',
+                studentsZids: 'z1234576,z1234577',
+                mentorsEmails: 'z2232324@unsw.edu.au'
+            },
+            {
+                teamName: 'jeffrey squad',
+                studentsZids: 'z5414078,z5423255,z5450260,z5349042,z5361545,z5417505,z5417509',
+                mentorsEmails: 'cowhorse3900@outlook.com,hehe_boi@bbc.co.uk'
+            },
+            {
+                teamName: 'child_prodigy',
+                studentsZids: 'z5322112,z5555555',
+                mentorsEmails: 'cowhorse3900@outlook.com,hehe_boi@bbc.co.uk'
+            }
+        ])
+    })
+
+    test('student_test: insert students', () => {
+        for (const row of convertedData) {
+            func.insertTeam(row, teams, newCourse)
+            const teamToAdd = teams.find(team => team.teamName === row.groupname)
+            func.insertTutor(row, staffAdmins, courseInfo, newCourse, teamToAdd!)
+            func.insertStudent(row, students, teamToAdd!)
+        }
+        expect(students).toStrictEqual([
+            {
+                studentName: 'piggie0',
+                email: 'z1234567@student.unsw.edu.au',
+                zid: 'z1234567'
+            },
+            {
+                studentName: 'piggie1',
+                email: 'z1234568@student.unsw.edu.au',
+                zid: 'z1234568'
+            },
+            {
+                studentName: 'piggie2',
+                email: 'z1234569@student.unsw.edu.au',
+                zid: 'z1234569'
+            },
+            {
+                studentName: 'piggie3',
+                email: 'z1234571@student.unsw.edu.au',
+                zid: 'z1234571'
+            },
+            {
+                studentName: 'piggie4',
+                email: 'z1234572@student.unsw.edu.au',
+                zid: 'z1234572'
+            },
+            {
+                studentName: 'doggie0',
+                email: 'z1234576@student.unsw.edu.au',
+                zid: 'z1234576'
+            },
+            {
+                studentName: 'doggie1',
+                email: 'z1234577@student.unsw.edu.au',
+                zid: 'z1234577'
+            },
+            {
+                studentName: 'Jeffrey',
+                email: 'belikov9519@gmail.com',
+                zid: 'z5414078'
+            },
+            {
+                studentName: 'Wilson',
+                email: 'wilsonzhu2003@gmail.com',
+                zid: 'z5423255'
+            },
+            {
+                studentName: 'Guojing',
+                email: 'z5450260@ad.unsw.edu.au',
+                zid: 'z5450260'
+            },
+            {
+                studentName: 'Rocky',
+                email: 'z5349042@ad.unsw.edu.au',
+                zid: 'z5349042'
+            },
+            {
+                studentName: 'Ruiqi',
+                email: 'z5361545@ad.unsw.edu.au',
+                zid: 'z5361545'
+            },
+            {
+                studentName: 'Waner',
+                email: 'z5417505@ad.unsw.edu.au',
+                zid: 'z5417505'
+            },
+        ])
+    })
+
+    test('insert courses', () => {
+        for (const row of convertedData) {
+            func.insertTeam(row, teams, newCourse)
+            const teamToAdd = teams.find(team => team.teamName === row.groupname)
+            func.insertTutor(row, staffAdmins, courseInfo, newCourse, teamToAdd!)
+            func.insertStudent(row, students, teamToAdd!)
+        }
+        expect(newCourse).toStrictEqual({
+            courseName: 'COMP3900',
+            mentorsEmails: 'z2232322@unsw.edu.au,z2232323@unsw.edu.au,z2232324@unsw.edu.au,cowhorse3900@outlook.com',
+            teams: 'piggie squad 0,piggie squad 1,doggie squad 0,jeffrey squad',
+            term: '24T3'
+        })
+    })
 })
