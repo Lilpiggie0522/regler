1450607517f2707de2d34945f070811c031bbb23
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-10-26 22:14:31 +1100

    Implemented use access control in a more structured manner, improved code quality and readability. linted and passed all tests

--- middleware.ts
+++ middleware.ts
@@ -1,69 +1,83 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { verifyJWT } from './util/jwt'
+import { JWTPayload } from 'jose'
 // This function can be marked `async` if using `await` inside
-const PROTECTED_ROUTES = {
-  "lecturer/.*": ["admin"]
-}
+const PROTECTED_ROUTES = [
+  {
+    pathRegex: "/lecturer.*",
+    access: ["admin"],
+    redirect: { tutor: '/staffCourseList', student: '/studentDetailConfirm', admin: '/' }
+  },
+  {
+    pathRegex: "/teamEvaluationForm.*",
+    access: ["student"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/' }
+  },
+  {
+    pathRegex: "/staffCourseList",
+    access: ["admin", "tutor"],
+    redirect: { tutor: '/', admin: '/', student: '/studentDetailConfirm' }
+  },
+  {
+    pathRegex: "/studentDetailConfirm",
+    access: ["student"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/' }
+  },
+  {
+    pathRegex: "/api/adminSystem/courses/.*",
+    access: ["admin", "tutor"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/studentDetailConfirm' }
+  },
+  {
+    pathRegex: "/api/adminSystem/initialise",
+    access: ["admin"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/studentDetailConfirm' }
+  },
+  {
+    pathRegex: "/api/issueSystem/.*",
+    access: ["student"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/studentDetailConfirm' }
+  },
+  {
+    pathRegex: "/api/staff/.*",
+    access: ["admin"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/studentDetailConfirm' }
+  },
+  {
+    pathRegex: "/api/util/.*",
+    access: ["admin", "tutor"],
+    redirect: { tutor: '/staffCourseList', admin: '/staffCourseList', student: '/studentDetailConfirm' }
+  }
+]
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value
   const path = request.nextUrl.pathname
-  try {
-    if (token) {
-      const {} = await verifyJWT(token)
-    }
-  } catch (error) {
-    
-  }
-  for (const [pathRegex, roles] of Object.entries(PROTECTED_ROUTES)) {
-    const compiledRegex = new RegExp(pathRegex)
+
+  for (const item of PROTECTED_ROUTES) {
+    const compiledRegex = new RegExp(item.pathRegex)
     if (path.match(compiledRegex)) {
-      
+      try {
+        const payload: JWTPayload = await verifyJWT(token)
+        const role: string = payload.role as string
+        if (!item.access.includes(role)) {
+          if (role === 'student') {
+            return NextResponse.redirect(new URL(item.redirect[role], request.url))
+          } else if (role === 'tutor') {
+            return NextResponse.redirect(new URL(item.redirect[role], request.url))
+          } else {
+            return NextResponse.redirect(new URL(item.redirect["admin"], request.url))
+          }
+        }
+      } catch (error) {
+        console.log("no token provided")
+        return NextResponse.redirect(new URL('/', request.url))
+      }
     }
   }
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*Login|Mainpage-prisci).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*Login|Mainpage-prisci).*)'],
 }
-
- // console.log(`route is: ${request.url}`)
-  // const token = request.cookies.get("token")?.value
-  // if (!token && request.nextUrl.pathname !== '/') {
-  //   console.log('no token, path not / redirect')
-  //   return NextResponse.redirect(new URL('/', request.url))
-  // }
-  // if (!token) {
-  //   return;
-  // }
-  // try {
-  //   const decoded = await verifyJWT(token)
-  //   if (request.nextUrl.pathname === '/' && (decoded.role === 'admin' || decoded.role === 'tutor')) {
-  //     return NextResponse.redirect(new URL('/staffCourseList', request.url))
-  //   } else if (request.nextUrl.pathname === '/' && decoded.role === 'student') {
-  //     return NextResponse.redirect(new URL('/studentDetailConfirm', request.url))
-  //   }
-  //   // route guard for admin page
-  //   if (request.nextUrl.pathname === '/lecturer' && decoded.role !== 'admin') {
-  //     if (decoded.role === 'student') {
-  //       return NextResponse.redirect(new URL('/studentDetailConfirm', request.url))
-  //     } else if (decoded.role === 'tutor') {
-  //       return NextResponse.redirect(new URL('/staffCourseList', request.url))  
-  //     }
-  //     return NextResponse.redirect(new URL('/', request.url))  
-  //   }
-
-  //   // route guard for staffCourseList page
-  //   if (request.nextUrl.pathname === '/staffCourseList' && decoded.role !== 'admin' && decoded.role !== 'tutor') {
-  //     return NextResponse.redirect(new URL('/', request.url))  
-  //   }
-  //   return NextResponse.next()
-  // } catch (error) {
-  //   console.log("something wrong mate!")
-  //   console.error("there is unexpected error: " + error);
-  //   if (request.nextUrl.pathname !== '/') {
-  //     console.log('no token, path not2 / redirect 22')
-  //     return NextResponse.redirect(new URL('/', request.url))
-  //   }
-  // }
