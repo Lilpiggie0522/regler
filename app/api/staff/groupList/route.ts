c33eed892fef5a3ecc6a942814aef345f1862863
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-10-30 15:08:37 +1100

    Implemented backend for groupList

--- app/api/staff/groupList/route.ts
+++ app/api/staff/groupList/route.ts
@@ -4,28 +4,30 @@ import models from "@/models/models";
 
 const Admin = models.Admin;
 const Course = models.Course;
+const Team = models.Team;
 // get courseById
 export async function POST(request : NextRequest) {
     try {
-        const { email } : { email: string }   = await request.json();
+        const result = []
+        const { email, courseName, term } : { email: string, courseName: string, term: string }   = await request.json();
         await dbConnect();
         const admin = await Admin.findOne({email: email})
         if (!admin) {
             return NextResponse.json("invalid staff email", {status: 401})
         }
-        const courses = admin.courses;
-        const groupList = [];
-        for (const course of courses) {
-            const currentCourse = await Course.findById(course).exec();
-            const teams = currentCourse.teams;
-            for (const team of teams) {
-                const teamObj = await models.Team.findById(team).exec();
-                if (admin.role === "admin" || (teamObj.mentors.includes(admin._id) && admin.role === "tutor")) {
-                    groupList.push(team);
-                }
+        const course = await Course.findOne({courseName: courseName, term: term})
+        if (!course) {
+            return NextResponse.json("invalid course and term", {status: 401})
+        }
+        const course_objId = course._id
+        const lecturer = await Admin.findOne({ courses: { $in: [course_objId] }, role: 'admin' });
+        console.log(`lecturer name is: ${lecturer.adminName}`)
+        for (const team of course.teams) {
+            const teamFound = await Team.findById(team)
+            if (teamFound) {
             }
         }
-        return NextResponse.json({teams: groupList}, {status: 200});
+        return NextResponse.json({email, courseName, term}, {status: 200});
     } catch (error) {
         return NextResponse.json({ error: error}, {status: 500});
     }
