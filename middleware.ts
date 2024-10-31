dfd7d89c2453771d1126ae17c01c13d8666a7ffb
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-10-31 16:02:08 +1100

    fixing bugs when initialising

--- middleware.ts
+++ middleware.ts
@@ -44,10 +44,10 @@ const PROTECTED_APIs = [
     pathRegex: "/api/adminSystem/courses/.*",
     access: ["admin", "tutor"]
   },
-  {
-    pathRegex: "/api/adminSystem/initialise",
-    access: ["admin"]
-  },
+  // {
+  //   pathRegex: "/api/adminSystem/initialise",
+  //   access: ["admin"]
+  // },
   {
     pathRegex: "/api/issueSystem/.*",
     access: ["student"]
@@ -56,10 +56,10 @@ const PROTECTED_APIs = [
     pathRegex: "/api/staff/readCsv",
     access: ["admin"]
   },
-  {
-    pathRegex: "/api/staff/.*",
-    access: ["admin", "tutor"]
-  },
+  // {
+  //   pathRegex: "/api/staff/.*",
+  //   access: ["admin", "tutor"]
+  // },
   {
     pathRegex: "/api/tutorOpinions/.*",
     access: ["admin", "tutor"]
