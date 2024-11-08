ed72710c4c06235ec482d25ff8f3da53b4dfd6cc
Ruiqi Diao <z5361545@ad.unsw.edu.au>
2024-11-08 14:13:56 +1100

    delete deleteReminder api and remove console log

--- app/api/mailingSystem/setReminder/route.ts
+++ app/api/mailingSystem/setReminder/route.ts
@@ -1,7 +1,7 @@
 import dbConnect from '@/lib/dbConnect';
 import reminderMod from '@/lib/reminderMod';
 import models from "@/models/models";
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import cron from 'node-cron';
 
 // const CHECK_TIME = '';
@@ -54,7 +54,6 @@ async function InitialReminderCron() {
     await reminderRequest();
     // cron.schedule('25 4 * * *', async () => {
     cron.schedule('*/5 * * * *', async () => {
-        console.log('Check every 5 minutes');
         await reminderRequest();
     });
 }
@@ -64,54 +63,6 @@ export async function POST() {
         await InitialReminderCron();
         return NextResponse.json({ message: "Reminder Initialised" }, { status: 200 });
     } catch (error) {
-        console.log(error);
         return NextResponse.json({ error: error }, { status: 404 });
-    }
-
-}
-
-// /*
-//     Overview:
-//     This function deletes job and update database when student submits form
-//     Input: 
-//         - id: Unique object id (team/student/course/issue)
-//         - type: strings to indicate type of id
-//     Output: 
-//         Delete object if id in (team/course/issue)
-//         Delete student if id in student
-//     Error:
-//         - 
-// */
-export async function DELETE(request: NextRequest) {
-    try {
-        await dbConnect();
-        const { issueId, personId, type } = await request.json();
-        
-        // check if mentor or student exists
-        const reminder = await models.Reminder.findOne({ issue: issueId });
-        if (!reminder) {
-            return NextResponse.json({ error: 'reminder not found or issue closed' }, { status: 400 });
-        }
-        if (type === "student") {
-            const student = await models.Student.findById(personId);
-            if (!student) {
-                return NextResponse.json({ error: 'student not in the team/ already submitted' }, { status: 400 })
-            }
-            await models.Reminder.updateOne({ issue: issueId }, { $pull: { students: personId } });
-        }
-        if (type === "mentor") {
-            const mentor = await models.Admin.findById({ _id: personId });
-            if (!mentor) {
-                return NextResponse.json({ error: 'tutor not in the team/ already submitted' }, { status: 400 })
-            }
-            await models.Reminder.updateOne({ issue: issueId }, { $pull: { mentors: personId } });
-        }
-
-        return NextResponse.json({ message: `Successfully remove ${personId} from ${issueId}` }, { status: 200 }) 
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error('Error - setReminder (DELETE)', error);
-            return NextResponse.json({ error: error.message }, { status: 502 })
-        }
     }
 }
