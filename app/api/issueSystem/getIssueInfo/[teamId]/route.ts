3226436abf5ca3e76e7b8be4e7e6c4bff4b5102c
wilson <wilsonzhu2003@gmail.com>
2024-11-08 16:23:03 +1100

    fix linting

--- app/api/issueSystem/getIssueInfo/[teamId]/route.ts
+++ app/api/issueSystem/getIssueInfo/[teamId]/route.ts
@@ -80,14 +80,12 @@ export async function GET(req : NextRequest, { params } : Params) {
                 zid: studentDetails.email.split("@")[0],
                 isSubmitted,
                 comment: isSubmitted ? {
-                    title: studentComment.title,
-                    content: studentComment.content,
+
                     filesUrl: studentComment.filesUrl,
                     filesName: studentComment.filesName,
                     student: studentId
                 } : {
-                    title:"not submitted", 
-                    content: "not submitted", 
+
                     filesUrl: "not submitted",
                     filesName: "not submitted",
                     student: studentId
